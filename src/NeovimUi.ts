5415d3f36c7eece6efb856aaf738d008a987dbab
glacambre <code@lacamb.re>
2019-03-06 20:04:18 +0100

    Compute better dimensions for frame, position it correctly in page

--- src/NeovimUi.ts
+++ src/NeovimUi.ts
@@ -1,16 +1,30 @@
 import { neovim } from "./Neovim";
 
 window.addEventListener("load", async () => {
-    const nvim = await neovim(document.getElementById("pre") as HTMLPreElement);
-    await nvim.ui_attach(80, 20, {
-        ext_linegrid: true,
-        rgb: true,
-    });
-    window.addEventListener("keydown", (evt) => {
-        if (evt.isTrusted) {
-            nvim.input(evt.key);
-            evt.preventDefault();
-            evt.stopImmediatePropagation();
-        }
+    const host = document.getElementById("pre") as HTMLPreElement;
+    const nvim = neovim(host);
+
+    // We need to know how wide our characters are
+    const span = document.createElement("span");
+    span.innerText = " ";
+    host.appendChild(span);
+    const { width: charWidth, height: charHeight } = span.getBoundingClientRect();
+    host.removeChild(span);
+    const rect = host.getBoundingClientRect();
+    const cols = Math.floor(rect.width / charWidth);
+    const rows = Math.floor(rect.height / charHeight);
+
+    nvim.then(n => {
+        n.ui_attach(cols, rows, {
+            ext_linegrid: true,
+            rgb: true,
+        });
+        window.addEventListener("keydown", (evt) => {
+            if (evt.isTrusted) {
+                n.input(evt.key);
+                evt.preventDefault();
+                evt.stopImmediatePropagation();
+            }
+        });
     });
 });
--- src/content.ts
+++ src/content.ts
@@ -1,13 +1,13 @@
 
 function nvimify(evt: Event) {
     const elem = evt.target as HTMLElement;
-    const [rect, _] = elem.getClientRects();
+    const rect = elem.getBoundingClientRect();
     const iframe = elem.ownerDocument
         .createElementNS("http://www.w3.org/1999/xhtml", "iframe") as HTMLIFrameElement;
     iframe.style.height = `${rect.height}px`;
-    iframe.style.left = `${rect.left}px`;
+    iframe.style.left = `${rect.left + window.scrollX}px`;
     iframe.style.position = "absolute";
-    iframe.style.top = `${rect.top}px`;
+    iframe.style.top = `${rect.top + window.scrollY}px`;
     iframe.style.width = `${rect.width}px`;
     iframe.src = (browser as any).extension.getURL("/NeovimFrame.html");
     const span = iframe.ownerDocument
