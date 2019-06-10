acccfb52f732a035970ee04654e29497d39b1074
glacambre <code@lacamb.re>
2019-06-10 15:25:16 +0200

    Put saveable text areas in their own components

--- src/preferences/ListInput.ts
+++ src/preferences/ListInput.ts
@@ -1,11 +1,10 @@
 import * as React from "react";
+import { SaveableTextArea } from "./SaveableTextArea";
 
 export class ListInput extends React.Component {
     public state: {
         checked: boolean,
         kind: "whitelist" | "blacklist",
-        savedRules: string,
-        stagedRules: string,
     };
     private messages = {
         "Enable Firenvim": {
@@ -19,24 +18,13 @@ export class ListInput extends React.Component {
         this.state = {
             checked: false,
             kind: props.kind,
-            savedRules: "example\\.{org,com}\nexample.net",
-            stagedRules: "example\\.{org,com}\nexample.net",
         };
         browser.storage.sync.get("selected").then(({ selected }: { selected: string }) => {
             this.setState({ checked: selected === this.state.kind });
         });
-        browser.storage.sync.get(this.state.kind).then((list: any) => {
-            this.setState({ savedRules: list[this.state.kind], stagedRules: list[this.state.kind] });
-        });
         browser.storage.onChanged.addListener((changes, areaname) => {
             if (changes.selected) {
                 this.setState({ checked: changes.selected.newValue === this.state.kind });
-            }
-            if (changes[this.state.kind]) {
-                this.setState({
-                    savedRules: changes[this.state.kind].newValue,
-                    stagedRules: changes[this.state.kind].newValue,
-                });
             }
         });
 
@@ -50,29 +38,7 @@ export class ListInput extends React.Component {
         }
     }
 
-    public onTextInput(e: React.FormEvent<HTMLElement>) {
-        this.setState({
-            stagedRules: (e.target as HTMLTextAreaElement).value,
-        });
-    }
-
-    public saveStagedRules() {
-        const obj: any = {};
-        obj[this.state.kind] = this.state.stagedRules;
-        browser.storage.sync.set(obj);
-    }
-
     public render() {
-        let saveButton;
-        if (this.state.savedRules !== this.state.stagedRules) {
-            saveButton = React.createElement("input", {
-                className: "saveButton",
-                onClick: (e) => this.saveStagedRules(),
-                type: "button",
-                value: "Save changes",
-            });
-        }
-
         const id = this.state.kind + "checkbox";
         return React.createElement("div", { id: this.state.kind, className: "listoption" },
             React.createElement("input", {
@@ -85,12 +51,11 @@ export class ListInput extends React.Component {
             }),
             React.createElement("label", { htmlFor: id },
                 this.messages["Enable Firenvim"][this.state.kind]),
-            React.createElement("textarea", {
+            React.createElement(SaveableTextArea, {
+                configName: this.state.kind,
+                defaultContent: "example\\.{org,com}\nexample.net",
                 disabled: !this.state.checked,
-                onInput: e => this.onTextInput(e),
-                value: this.state.stagedRules,
             }),
-            saveButton,
         );
     }
 }
---
+++ src/preferences/SaveableTextArea.ts
@@ -0,0 +1,67 @@
+import * as React from "react";
+
+export class SaveableTextArea extends React.Component {
+    public state: {
+        configName: string,
+        currentContent: string,
+        disabled: boolean,
+        savedContent: string,
+    };
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            configName: props.configName,
+            currentContent: props.defaultContent,
+            disabled: props.disabled,
+            savedContent: props.defaultContent,
+        };
+        browser.storage.sync.get(this.state.configName).then((list: any) => {
+            this.setState({
+                currentContent: list[this.state.configName],
+                savedContent: list[this.state.configName],
+            });
+        });
+        browser.storage.onChanged.addListener((changes, areaname) => {
+            if (changes[this.state.configName]) {
+                this.setState({
+                    currentContent: changes[this.state.configName].newValue,
+                    savedContent: changes[this.state.configName].newValue,
+                });
+            }
+        });
+    }
+
+    public onTextInput(e: React.FormEvent<HTMLElement>) {
+        this.setState({
+            currentContent: (e.target as HTMLTextAreaElement).value,
+        });
+    }
+
+    public saveContent() {
+        const obj: any = {};
+        obj[this.state.configName] = this.state.currentContent;
+        browser.storage.sync.set(obj);
+    }
+
+    public render() {
+        let saveButton;
+        if (this.state.savedContent !== this.state.currentContent) {
+            saveButton = React.createElement("input", {
+                className: "saveButton",
+                onClick: (e) => this.saveContent(),
+                type: "button",
+                value: "Save changes",
+            });
+        }
+
+        return React.createElement("div", {},
+            React.createElement("textarea", {
+                disabled: (this.props as any).disabled,
+                onInput: e => this.onTextInput(e),
+                value: this.state.currentContent,
+            }),
+            saveButton,
+        );
+    }
+}
