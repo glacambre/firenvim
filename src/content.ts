591f2a1e44f990a014be8f4792a8d1930a2c7616
glacambre <code@lacamb.re>
2019-06-12 19:54:28 +0200

    content.ts: iframe disappears when elem disappears, resize more sensibly
    Closes #23 and #18.

--- src/content.ts
+++ src/content.ts
@@ -16,6 +16,7 @@ const global = {
         global.selectorToElems.set(selector, pageElements);
 
         global.lastEditorLocation = [document.location.href, selector];
+        // We use a span because these are the least likely to disturb the page
         const span = elem.ownerDocument
             .createElementNS("http://www.w3.org/1999/xhtml", "span") as HTMLSpanElement;
         pageElements.span = span;
@@ -25,17 +26,43 @@ const global = {
             .createElementNS("http://www.w3.org/1999/xhtml", "iframe") as HTMLIFrameElement;
         pageElements.iframe = iframe;
 
+        // We don't need the iframe to be appended to the page in order to
+        // resize it because we're just using the corresponding
+        // input/textarea's size
         resizeEditor(pageElements);
-        window.addEventListener("resize", _ => resizeEditor(pageElements));
+        // Resizing a textarea changes its "style" attribute
+        // This is a hack. We should ideally use a ResizeObserver (
+        // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver )
+        // but this API doesn't exist in Firefox yet :(
+        new MutationObserver((changes, observer) => resizeEditor(pageElements))
+            .observe(elem, { attributes: true, attributeFilter: ["style"] });
+
         iframe.src = (browser as any).extension.getURL("/NeovimFrame.html");
         span.attachShadow({ mode: "closed" }).appendChild(iframe);
         elem.ownerDocument.body.appendChild(span);
+
+        // Some inputs try to grab the focus again after we appended the iframe
+        // to the page, so we need to refocus it each time it loses focus. But
+        // the user might want to stop focusing the iframe at some point, so we
+        // actually stop refocusing the iframe a second after it is created.
         function refocus() {
             setTimeout(() => iframe.focus(), 0);
         }
         iframe.addEventListener("blur", refocus);
         setTimeout(() => iframe.removeEventListener("blur", refocus), 1000);
         refocus();
+
+        // We want to remove the frame from the page if the corresponding
+        // element has been removed. It is pretty hard to tell when an element
+        // disappears from the page (either by being removed or by being hidden
+        // by other elements), so we use an intersection observer, which is
+        // triggered every time the element becomes more or less visible.
+        (new IntersectionObserver((entries, observer) => {
+            if (!elem.ownerDocument.contains(elem)
+                || (elem.offsetWidth === 0 && elem.offsetHeight === 0 && elem.getClientRects().length === 0)) {
+                functions.killEditor(selector);
+            }
+        }, { root: null, threshold: 0.1 })).observe(elem);
     },
     selectorToElems: new Map<string, PageElements>(),
 };
@@ -71,26 +98,16 @@ function addNvimListener(elem: Element) {
     elem.addEventListener("focus", global.nvimify);
 }
 
-const mutationTimeout = 0;
-
 function setupListeners(selector: string) {
-    (new MutationObserver(changes => {
+    (new MutationObserver((changes, observer) => {
+        if (changes.filter(change => change.addedNodes.length > 0).length <= 0) {
+            return;
+        }
+        // This mutation observer is triggered every time an element is
+        // added/removed from the page. When this happens, try to apply
+        // listeners again, in case a new textarea/input field has been added.
         Array.from(document.querySelectorAll(selector))
             .forEach(elem => addNvimListener(elem));
-        // Each time nodes have been removed from the page, check if each of our
-        // iframes should be removed from the page. This would be wasteful for
-        // large numbers of iframes but we'll never have more than 10 anyway so
-        // it's probably ok.
-        if (changes.find(change => change.removedNodes.length > 0)) {
-            global.selectorToElems.forEach(({input: elem}, select, map) => {
-                // If element is not in document or is not visible
-                if (!elem.ownerDocument.contains(elem)
-                    || (elem.offsetWidth === 0 && elem.offsetHeight === 0 && elem.getClientRects().length === 0)) {
-                    functions.killEditor(select);
-                }
-            });
-        }
-        global.selectorToElems.forEach(resizeEditor);
     })).observe(window.document, { subtree: true, childList: true });
 
     Array.from(document.querySelectorAll(selector))
