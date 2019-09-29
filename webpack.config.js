424850a5bcad2a0b19c97df87b7aa574d5faf467
glacambre <code@lacamb.re>
2019-09-29 07:24:25 +0200

    webpack.config.js: populate chrome manifest with right key for testing

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,8 @@ module.exports = [
       transform: (content, src) => {
         switch(path.basename(src)) {
           case "manifest.json":
-            return content.toString().replace("BROWSER_SPECIFIC_SETTINGS,", ``)
+            return content.toString()
+              .replace('BROWSER_SPECIFIC_SETTINGS,', '"key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk3pkgh862ElxtREZVPLxVNbiFWo9SnvZtZXZavNvs2GsUTY/mB9yHTPBGJiBMJh6J0l+F5JZivXDG7xdQsVD5t39CL3JGtt93M2svlsNkOEYIMM8tHbp69shNUKKjZOfT3t+aZyigK2OUm7PKedcPeHtMoZAY5cC4L1ytvgo6lge+VYQiypKF87YOsO/BGcs3D+MMdS454tLBuMp6LxMqICQEo/Q7nHGC3eubtL3B09s0l17fJeq/kcQphczKbUFhTVnNnIV0JX++UCWi+BP4QOpyk5FqI6+SVi+gxUosbQPOmZR4xCAbWWpg3OqMk4LqHaWpsBfkW9EUt6EMMMAfQIDAQAB",\n')
               .replace("FIRENVIM_VERSION", package_json.version)
               .replace("PACKAGE_JSON_DESCRIPTION", package_json.description)
               // Chrome doesn't support svgs in its manifest
