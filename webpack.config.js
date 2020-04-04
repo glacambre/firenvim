9ccbead1198e4089000ae76b73b72a618c05f8f4
glacambre <code@lacamb.re>
2020-04-04 10:15:53 +0200

    webpack.config.js: allow building for each target separately

--- webpack.config.js
+++ webpack.config.js
@@ -61,64 +61,71 @@ const package_json = JSON.parse(require("fs").readFileSync(path.join(__dirname, 
 const chrome_target_dir = path.join(__dirname, "target", "chrome")
 const firefox_target_dir = path.join(__dirname, "target", "firefox")
 
-module.exports = [
-  Object.assign(deepCopy(config), {
-    output: {
-      path: chrome_target_dir,
+const chromeConfig = Object.assign(deepCopy(config), {
+  output: {
+    path: chrome_target_dir,
+  },
+  plugins: [new CopyWebPackPlugin(CopyWebPackFiles.map(file => ({
+    from: file,
+    to: chrome_target_dir,
+    transform: (content, src) => {
+      if (path.basename(src) === "manifest.json") {
+        content = content.toString()
+          .replace('BROWSER_SPECIFIC_SETTINGS', '"key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk3pkgh862ElxtREZVPLxVNbiFWo9SnvZtZXZavNvs2GsUTY/mB9yHTPBGJiBMJh6J0l+F5JZivXDG7xdQsVD5t39CL3JGtt93M2svlsNkOEYIMM8tHbp69shNUKKjZOfT3t+aZyigK2OUm7PKedcPeHtMoZAY5cC4L1ytvgo6lge+VYQiypKF87YOsO/BGcs3D+MMdS454tLBuMp6LxMqICQEo/Q7nHGC3eubtL3B09s0l17fJeq/kcQphczKbUFhTVnNnIV0JX++UCWi+BP4QOpyk5FqI6+SVi+gxUosbQPOmZR4xCAbWWpg3OqMk4LqHaWpsBfkW9EUt6EMMMAfQIDAQAB"')
+          .replace("FIRENVIM_VERSION", package_json.version)
+          .replace("PACKAGE_JSON_DESCRIPTION", package_json.description)
+        // Chrome doesn't support svgs in its manifest
+          .replace(/"128": *"firenvim\.svg"/g, '"128": "firenvim128.png",\n'
+            + '      "16": "firenvim16.png",\n'
+            + '      "48": "firenvim48.png"'
+          )
+          .replace(/"default_icon": "firenvim.svg"/, '"default_icon": "firenvim128.png"')
+        ;
+      }
+      return content;
     },
-    plugins: [new CopyWebPackPlugin(CopyWebPackFiles.map(file => ({
-      from: file,
-      to: chrome_target_dir,
-      transform: (content, src) => {
-        if (path.basename(src) === "manifest.json") {
-            content = content.toString()
-              .replace('BROWSER_SPECIFIC_SETTINGS', '"key": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk3pkgh862ElxtREZVPLxVNbiFWo9SnvZtZXZavNvs2GsUTY/mB9yHTPBGJiBMJh6J0l+F5JZivXDG7xdQsVD5t39CL3JGtt93M2svlsNkOEYIMM8tHbp69shNUKKjZOfT3t+aZyigK2OUm7PKedcPeHtMoZAY5cC4L1ytvgo6lge+VYQiypKF87YOsO/BGcs3D+MMdS454tLBuMp6LxMqICQEo/Q7nHGC3eubtL3B09s0l17fJeq/kcQphczKbUFhTVnNnIV0JX++UCWi+BP4QOpyk5FqI6+SVi+gxUosbQPOmZR4xCAbWWpg3OqMk4LqHaWpsBfkW9EUt6EMMMAfQIDAQAB"')
-              .replace("FIRENVIM_VERSION", package_json.version)
-              .replace("PACKAGE_JSON_DESCRIPTION", package_json.description)
-              // Chrome doesn't support svgs in its manifest
-              .replace(/"128": *"firenvim\.svg"/g, '"128": "firenvim128.png",\n'
-                + '      "16": "firenvim16.png",\n'
-                + '      "48": "firenvim48.png"'
-              )
-              .replace(/"default_icon": "firenvim.svg"/, '"default_icon": "firenvim128.png"')
-            ;
-        }
-        return content;
-      },
-    })).concat([16, 48, 128].map(n => ({
-        from: "static/firenvim.svg",
-        to: chrome_target_dir,
-        transformPath: (target) => `firenvim${n}.png`,
-        transform: (content, src) => sharp(content).resize(n, n).toBuffer(),
-      })
-    ))
-    )]
-  }),
-  Object.assign(deepCopy(config), {
-    output: {
-      path: firefox_target_dir,
-    },
-    plugins: [new CopyWebPackPlugin(CopyWebPackFiles.map(file => ({
-      from: file,
-      to: firefox_target_dir,
-      transform: (content, src) => {
-        switch(path.basename(src)) {
-          case "manifest.json":
-            return content.toString().replace("BROWSER_SPECIFIC_SETTINGS",
+  })).concat([16, 48, 128].map(n => ({
+    from: "static/firenvim.svg",
+    to: chrome_target_dir,
+    transformPath: (target) => `firenvim${n}.png`,
+    transform: (content, src) => sharp(content).resize(n, n).toBuffer(),
+  })
+  ))
+  )]
+});
+
+const firefoxConfig = Object.assign(deepCopy(config), {
+  output: {
+    path: firefox_target_dir,
+  },
+  plugins: [new CopyWebPackPlugin(CopyWebPackFiles.map(file => ({
+    from: file,
+    to: firefox_target_dir,
+    transform: (content, src) => {
+      switch(path.basename(src)) {
+        case "manifest.json":
+          return content.toString().replace("BROWSER_SPECIFIC_SETTINGS",
 `  "browser_specific_settings": {
     "gecko": {
       "id": "firenvim@lacamb.re",
       "strict_min_version": "69.0"
     }
   }`)
-              .replace("FIRENVIM_VERSION", package_json.version)
-              .replace("PACKAGE_JSON_DESCRIPTION", package_json.description)
-            ;
-            break;
-        }
-        return content;
+            .replace("FIRENVIM_VERSION", package_json.version)
+            .replace("PACKAGE_JSON_DESCRIPTION", package_json.description)
+          ;
+          break;
       }
-    })))]
-  }),
-];
+      return content;
+    }
+  })))]
+});
+
+module.exports = env => {
+  switch (env) {
+    case "chrome": return [chromeConfig];
+    case "firefox": return [firefoxConfig];
+  }
+  return [chromeConfig, firefoxConfig];
+}
 
