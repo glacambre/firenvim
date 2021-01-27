2d7ef97d58b3b2defde1e2485297d1b32d85ecd8
glacambre <code@lacamb.re>
2021-01-27 19:25:13 +0100

    coverage: ignore branch that can only be tested on OSX

--- src/frame.ts
+++ src/frame.ts
@@ -114,6 +114,18 @@ export const isReady = new Promise((resolve, reject) => {
 
             const ignoreKeys = settings.ignoreKeys;
             keyHandler.addEventListener("keydown", (evt) => {
+                // This is a workaround for osx where pressing non-alphanumeric
+                // characters like "@" requires pressing <A-a>, which results
+                // in the browser sending an <A-@> event, which we want to
+                // treat as a regular @.
+                // So if we're seeing an alt on a non-alphanumeric character,
+                // we just ignore it and let the input event handler do its
+                // magic. This can only be tested on OSX, as generating an
+                // <A-@> keydown event with selenium won't result in an input
+                // event.
+                // Since coverage reports are only retrieved on linux, we don't
+                // instrument this condition.
+                /* istanbul ignore next */
                 if (evt.altKey && settings.alt === "alphanum" && !/[a-zA-Z0-9]/.test(evt.key)) {
                     return;
                 }
