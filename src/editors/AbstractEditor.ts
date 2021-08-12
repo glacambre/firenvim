a919331362a3ff34c704dec8cb62be123e43b0ba
glacambre <code@lacamb.re>
2021-08-12 22:46:49 +0200

    Use external editor-adapter
    Firenvim's editors have been put in a library so that they can be shared
    with Tridactyl. This commit replaces the in-tree editor with the
    library.

--- src/editors/AbstractEditor.ts
+++
@@ -1,9 +0,0 @@
-
-export abstract class AbstractEditor {
-    public abstract getContent (): Promise<string>;
-    public abstract getCursor (): Promise<[number, number]>;
-    public abstract getElement (): HTMLElement;
-    public abstract getLanguage (): Promise<string | undefined>;
-    public abstract setContent (s: string): Promise<any>;
-    public abstract setCursor (line: number, column: number): Promise<any>;
-}
