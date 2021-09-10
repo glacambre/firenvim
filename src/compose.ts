70a962fd8949719d0b0fd93808037c168bb794a0
glacambre <code@lacamb.re>
2021-09-10 07:16:46 +0200

    thunderbird: hide remaining compose UI elements
    Closes #1176

--- src/compose.ts
+++ src/compose.ts
@@ -25,9 +25,21 @@ canvas.style.top = "0px";
 canvas.style.left = "0px";
 document.body.appendChild(canvas);
 
-// Remove scrollbars when body content is longer than window
-document.documentElement.style.overflow = "hidden";
-document.body.style.overflow = "hidden";
+const style = document.createElement("style");
+style.innerText = `
+html, body {
+    /* Hide caret, which sometimes appears over canvas */
+    caret-color: transparent !important;
+    /* Hide scrollbars when email is longer than window */
+    overflow: hidden !important;
+}
+body > *:not(canvas) {
+    /* Hide email content, useful when email is longer than canvas and */
+    /* canvas shorter than window */
+    display: none !important;
+}
+`.replace(/\n/g, "");
+document.head.appendChild(style);
 
 const connectionPromise = browser.runtime.sendMessage({ funcName: ["getNeovimInstance"] });
 
