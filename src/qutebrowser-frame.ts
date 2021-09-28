e72dadd51297feeac6baef271ef309fdb32726f7
glacambre <code@lacamb.re>
2021-09-28 21:08:54 +0200

    Even more qutebrowser support

--- src/qutebrowser-frame.ts
+++ src/qutebrowser-frame.ts
@@ -2,13 +2,44 @@ import { EventEmitter } from "./EventEmitter";
 import { PageEvents, PageHandlers } from "./page";
 import { KeydownHandler } from "./KeyHandler";
 import { setupInput } from "./input";
+import { applySettingsToDefaults } from "./utils/configuration";
 
+// QUTEBROWSER_PORT and QUTEBROWSER_PASSWORD will be replaced through
+// string-substitutions when the user runs `spawn --userscript firenvim`
 const connectionPromise = Promise.resolve({ port: "QUTEBROWSER_PORT", password: "QUTEBROWSER_PASSWORD" });
+
+// Tiny promise to allow blocking until page is loaded
 const pageLoaded = new Promise((resolve, reject) => {
     window.addEventListener("load", resolve);
     setTimeout(reject, 10000)
 });
 
+// Handler to notify the parent
+// TODO: see if we can find a way to only notify the qutebrowser script rather
+// than the whole window?
+function notify(data: object) {
+    window.parent.postMessage(data, "*");
+}
+
+// Notify and wait for an answer
+function request(data: object) {
+    const reqId = Math.random();
+    return new Promise((resolve) => {
+        function once(e: MessageEvent<any>) {
+            if (e.source !== window.parent || e.data.token !== (window as any).authToken) {
+                return
+            }
+            if (e.data.funcName === "resolve" && e.data.reqId === reqId) {
+                window.removeEventListener("message", once);
+                return resolve(e.data.args[0]);
+            }
+        }
+        window.addEventListener("message", once);
+        notify(Object.assign({ reqId }, data));
+    });
+}
+
+// An event emitter that takes care of sending messages to the frame and receiving answers
 class QutePageEventEmitter extends EventEmitter<PageEvents, PageHandlers> {
     private resizeCount = 0;
     constructor() {
@@ -22,57 +53,44 @@ class QutePageEventEmitter extends EventEmitter<PageEvents, PageHandlers> {
         // window might be 1px wide when the compose script is created.
         setTimeout(onResize, 100);
     }
+    // Let's not allow eval for now...
     async evalInPage(js: string) { console.error(`eval(${js})`); }
-    async focusInput() { return Promise.resolve(); }
-    async focusPage() { return Promise.resolve(); }
-    async getEditorInfo() { return [document.location.href, "", [1, 1], undefined] as [string, string, [number, number], string] }
+    async focusInput() {
+        notify({"funcName": "focusInput", args: []});
+    }
+    async focusPage() {
+        notify({"funcName": "focusPage", args: []});
+    }
+    async getEditorInfo() {
+        return request({"funcName": "getEditorInfo", args: []}) as Promise<[string, string, [number, number], string]>
+    }
     async getElementContent() {
-        return (window as any).editorContent || "fail";
+        return request({"funcName": "getElementContent", args: []}) as Promise<string>
     }
-    async hideEditor() { return Promise.resolve(); }
-    async killEditor() { console.error("killEditor"); }
+    async hideEditor() {
+        return notify({"funcName": "hideEditor", args: []})
+    }
+    async killEditor() {
+        return notify({"funcName": "killEditor", args: []})
+    }
+    // Let's avoid this one too until we have a proper idea of what this means security-wise
     async pressKeys(_: any[]) { return Promise.resolve(); }
-    async resizeEditor(_: number, __: number) { return Promise.resolve(); }
-    async setElementContent(text: string) {
-        window.parent.postMessage({"funcName": "setContent", args:[text]}, "*");
+    async resizeEditor(w: number, h: number) {
+        return notify({"funcName": "resizeEditor", args: [w, h]});
     }
-    async setElementCursor(_: number, __: number) { return Promise.resolve(); }
+    async setElementContent(text: string) {
+        notify({"funcName": "setElementContent", args: [text]});
+    }
+    async setElementCursor(x: number, y: number) {
+        notify({"funcName": "setElementCursor", args: [x]});
+    }
 }
 
-
+// Takes care of receiving keystrokes
 class QuteKeyHandler extends KeydownHandler {
 
     constructor(private keyHandler: HTMLElement) {
-        super(keyHandler, {
-            alt: "all",
-            "<C-n>": "noop",
-            "<C-t>": "noop",
-            "<C-w>": "noop",
-            "<CS-n>": "noop",
-            "<CS-t>": "noop",
-            "<CS-w>": "noop",
-            ignoreKeys: {
-                "all": [],
-                "normal": [],
-                "visual": [],
-                "insert": [],
-                "replace": [],
-                "cmdline_normal": [],
-                "cmdline_insert": [],
-                "cmdline_replace": [],
-                "operator": [],
-                "visual_select": [],
-                "cmdline_hover": [],
-                "statusline_hover": [],
-                "statusline_drag": [],
-                "vsep_hover": [],
-                "vsep_drag": [],
-                "more": [],
-                "more_lastline": [],
-                "showmatch": [],
-            },
-            cmdlineTimeout: 3000,
-        });
+        super(keyHandler, applySettingsToDefaults("", {}));
 
         const acceptInput = ((evt: any) => {
             this.emit("input", evt.target.value);
--- src/qutebrowser.ts
+++ src/qutebrowser.ts
@@ -1,21 +1,70 @@
+import { computeSelector } from "./utils/utils";
 import { getEditor } from "editor-adapter";
 
+const iframe = document.createElement("iframe");
+
+// Create auth token
+const random = new Uint32Array(8);
+window.crypto.getRandomValues(random);
+const token = Array.from(random).join("");
+
+// Get abstracteditor for focused element
 const editor = getEditor(document.activeElement as HTMLElement, {});
 
-window.addEventListener("message", e => {
-    console.log(e)
+function reply(e: MessageEvent<any>, rep: any) {
+    e.source.postMessage({
+        "funcName": "resolve",
+        "args": [rep],
+        "reqId": e.data.reqId,
+        "token": token,
+    }, "*" as WindowPostMessageOptions);
+}
+
+// Start listening for message telling us to write to textarea
+window.addEventListener("message", async (e) => {
     if (e.origin !== document.location.origin) {
-        console.log("returning", e.origin, document.location.origin);
         return
     }
-    if (e.data.funcName === "setContent") {
-        editor.setContent(e.data.args[0]);
+    switch (e.data.funcName) {
+        case "focusInput":
+            editor.getElement().focus();
+            break;
+        case "focusPage":
+            document.documentElement.focus();
+            break;
+        case "getEditorInfo":
+            const cursor = await editor.getCursor();
+            const lang = await editor.getLanguage();
+            reply(e, [document.location.href, computeSelector(editor.getElement()), cursor, lang]);
+            break;
+        case "getElementContent":
+            const content = await editor.getContent();
+            reply(e, content);
+            break;
+        case "hideEditor":
+            // Needs more thinking
+            break;
+        case "killEditor":
+            iframe.remove()
+            break;
+        case "resizeEditor":
+            iframe.width = e.data.args[0];
+            iframe.height = e.data.args[1];
+        break;
+        case "setElementContent":
+            editor.setContent(e.data.args[0]);
+        break;
+        case "setElementCursor":
+            editor.setCursor(e.data.args[0], e.data.args[1]);
+        break;
     }
 });
 
 editor.getContent().then(content => {
-    const tag = `<script>window.editorContent = ${JSON.stringify(content)}</script>`;
-    const iframe = document.createElement("iframe");
+    // Create script tag out of token for insertion in frame HTML.
+    // The reason we do this rather than use postMessage is that we can't trust
+    // any postMessage without the token.
+    const tag = `<script>window.authToken = "${token}"</script>`;
     const blob = new Blob([tag, /* QUTEBROWSER_PAGE */], {type: "text/html; charset=utf-8"});
     iframe.src = URL.createObjectURL(blob);
     document.documentElement.appendChild(iframe);
