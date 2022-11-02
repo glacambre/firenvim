68c76638d4ea6eb92e66e19a941b91ba795b8423
glacambre <code@lacamb.re>
2022-11-02 11:28:32 +0100

    Post-thunderbird removal refactor

--- src/frame.ts
+++ src/frame.ts
@@ -1,64 +1,13 @@
-import { KeydownHandler } from "./KeyHandler";
-import { confReady, getGlobalConf, GlobalSettings } from "./utils/configuration";
+import { confReady, getGlobalConf } from "./utils/configuration";
 import { getPageProxy } from "./page";
-import { isChrome } from "./utils/utils";
 import { setupInput } from "./input";
+import { KeyHandler } from "./KeyHandler";
 
-const connectionPromise = browser.runtime.sendMessage({ funcName: ["getNeovimInstance"] });
 const pageLoaded = new Promise((resolve, reject) => {
     window.addEventListener("load", resolve);
     setTimeout(reject, 10000)
 });
-
-class BrowserKeyHandler extends KeydownHandler {
-
-    constructor(private keyHandler: HTMLElement, settings: GlobalSettings) {
-        super(keyHandler, settings);
-
-        const acceptInput = ((evt: any) => {
-            this.emit("input", evt.target.value);
-            evt.preventDefault();
-            evt.stopImmediatePropagation();
-        }).bind(this);
-
-        this.keyHandler.addEventListener("input", (evt: any) => {
-            if (evt.isTrusted && !evt.isComposing) {
-                acceptInput(evt);
-                evt.target.innerText = "";
-                evt.target.value = "";
-            }
-        });
-
-        // On Firefox, Pinyin input method for a single chinese character will
-        // result in the following sequence of events:
-        // - compositionstart
-        // - input (character)
-        // - compositionend
-        // - input (result)
-        // But on Chrome, we'll get this order:
-        // - compositionstart
-        // - input (character)
-        // - input (result)
-        // - compositionend
-        // So Chrome's input event will still have its isComposing flag set to
-        // true! This means that we need to add a chrome-specific event
-        // listener on compositionend to do what happens on input events for
-        // Firefox.
-        // Don't instrument this branch as coverage is only generated on
-        // Firefox.
-        /* istanbul ignore next */
-        if (isChrome()) {
-            this.keyHandler.addEventListener("compositionend", (e: CompositionEvent) => {
-                acceptInput(e);
-            });
-        }
-    }
-
-    moveTo(x: number, y: number) {
-        this.keyHandler.style.left = `${x}px`;
-        this.keyHandler.style.top = `${y}px`;
-    }
-}
+const connectionPromise = browser.runtime.sendMessage({ funcName: ["getNeovimInstance"] });
 
 export const isReady = browser
     .runtime
@@ -69,6 +18,6 @@ export const isReady = browser
         return setupInput(
             getPageProxy(frameId),
             document.getElementById("canvas") as HTMLCanvasElement,
-            new BrowserKeyHandler(document.getElementById("keyhandler"), getGlobalConf()),
+            new KeyHandler(document.getElementById("keyhandler"), getGlobalConf()),
             connectionPromise);
     });
