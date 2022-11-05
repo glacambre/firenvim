83b6dd8fc60152bcae5ddeafa3a538cc375b5ae1
glacambre <code@lacamb.re>
2022-11-05 10:01:36 +0100

    release.sh: fix git-archive step

--- release.sh
+++ release.sh
@@ -113,7 +113,7 @@ sed 's/"key":\s*"[^"]*",//' -i target/chrome/manifest.json
 # Generate bundles that need to be uploaded to chrome/firefox stores
 rm -f target/chrome.zip
 zip --junk-paths target/chrome.zip target/chrome/*
-git archive "$newVersion" > target/firenvim-firefox-sources.tar.gz
+git archive "v$newVersion" > target/firenvim-firefox-sources.tar.gz
 
 # Everythign went fine, we can push
 git push
