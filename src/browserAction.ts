302a6fe217366a10f20ee8061337e023af04bd4f
Ghjuvan Lacambre <code@lacamb.re>
2025-09-13 14:29:50 +0200

    Revert "fix: update browserAction to use MessageType system"
    This reverts commit eaf5197dffbc858b59e55184bb8e171ccaf86035.

--- src/browserAction.ts
+++ src/browserAction.ts
@@ -1,26 +1,29 @@
 
-import { MessageType } from "./MessageTypes";
-
-function displayMessages(messageType: MessageType.GET_ERROR | MessageType.GET_WARNING, id: "errors" | "warnings") {
+function displayMessages(func: "getError" | "getWarning", id: "errors" | "warnings") {
     function insertMessage(msg: any) {
         document.getElementById(id).innerText = msg;
     }
-    return browser.runtime.sendMessage({ type: messageType })
+    return browser.runtime.sendMessage({ funcName: [func] })
         .then(insertMessage)
         .catch(insertMessage);
 }
 
 function displayErrorsAndWarnings() {
-    return Promise.all([displayMessages(MessageType.GET_WARNING, "warnings"), displayMessages(MessageType.GET_ERROR, "errors")]);
+    return Promise.all([displayMessages("getWarning", "warnings"), displayMessages("getError", "errors")]);
 }
 
 async function updateDisableButton() {
-    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-    const tabId = tabs[0].id;
-    const disabled = await browser.runtime.sendMessage({
-        type: MessageType.GET_TAB_VALUE_FOR,
+    const tabId = (await browser.runtime.sendMessage({
+        args: {
+            args: [{ active: true, currentWindow: true }],
+            funcName: [ "browser", "tabs", "query" ],
+        },
+        funcName: ["exec"],
+    }))[0].id;
+    const disabled = (await browser.runtime.sendMessage({
         args: [tabId, "disabled"],
-    });
+        funcName: ["getTabValueFor"],
+    }));
     const button = document.getElementById("disableFirenvim");
     if (disabled === true) {
         button.innerText = "Enable in this tab";
@@ -31,16 +34,16 @@ async function updateDisableButton() {
 
 addEventListener("DOMContentLoaded", () => {
     document.getElementById("reloadSettings").addEventListener("click", () => {
-        browser.runtime.sendMessage({ type: MessageType.UPDATE_SETTINGS })
+        browser.runtime.sendMessage( { funcName: ["updateSettings"] })
             .then(displayErrorsAndWarnings)
             .catch(displayErrorsAndWarnings);
     });
     document.getElementById("disableFirenvim").addEventListener("click", () => {
-        browser.runtime.sendMessage({ type: MessageType.TOGGLE_DISABLED })
+        browser.runtime.sendMessage( { funcName: ["toggleDisabled"] })
             .then(updateDisableButton);
     });
     document.getElementById("troubleshooting").addEventListener("click", () => {
-        browser.runtime.sendMessage({ type: MessageType.OPEN_TROUBLESHOOTING_GUIDE });
+        browser.runtime.sendMessage( { funcName: ["openTroubleshootingGuide"] });
     })
     displayErrorsAndWarnings();
     updateDisableButton();
