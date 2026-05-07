b434efaea5fc2dc368720107ec2a7d2fcc535fc7
glacambre <code@lacamb.re>
2026-05-07 19:19:47 +0200

    testing: stop attempting to toggle extension in incognito mode in chrome

--- tests/chrome.ts
+++ tests/chrome.ts
@@ -144,20 +144,6 @@ describe("Chrome", () => {
                 background = await backgroundPromise;
                 await driver.sleep(1000);
 
-                // Now we need to enable the extension in incognito mode if
-                // it's not enabled. This is required for the browser keyboard
-                // shortcut fallback test.
-                await driver.get("chrome://extensions/?id=egpjdkipkomnmjhjmdamaniclmdlobbo");
-                let incognitoToggle = "document.querySelector('extensions-manager').shadowRoot.querySelector('#viewManager > extensions-detail-view.active').shadowRoot.querySelector('div#container.page-container > div.page-content > div#options-section extensions-toggle-row#allow-incognito').shadowRoot.querySelector('label#label input')";
-                const mustToggle = await driver.executeScript(`return !${incognitoToggle}.checked`);
-                if (mustToggle) {
-                        // Extension is going to be reloaded when enabling incognito mode, so be prepared
-                        backgroundPromise = coverageServer.getNextBackgroundConnection();
-                        await driver.sleep(1000);
-                        await driver.executeScript(`${incognitoToggle}.click()`);
-                        await driver.sleep(1000);
-                        background = await backgroundPromise;
-                }
                 return await loadLocalPage(server, driver, "simple.html", "");
         }, 120000);
 
