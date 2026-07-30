701b8ced08b072d74b37324a9912ee4de11768ea
Propfend <luiz13miguel24@gmail.com>
2026-07-30 10:58:01 -0300

    test: add the data dir on vimrc setup command for tests
    append nvim to the data dir used in tests

--- tests/_vimrc.ts
+++ tests/_vimrc.ts
@@ -6,19 +6,29 @@ const process = require("process");
 let init_vim: string;
 
 export function setupVimrc() {
-        const base_dir = path.join(os.tmpdir(), "firenvim_test_run", `${Math.round(Math.random() * 100000)}`);
+        const base_dir = path.join(
+                os.tmpdir(),
+                "firenvim_test_run",
+                `${Math.round(Math.random() * 100000)}`,
+        );
+
         process.env.XDG_CONFIG_HOME = path.join(base_dir, "config");
         process.env.XDG_DATA_HOME = path.join(base_dir, "data");
+
         const nvim_conf_dir = path.join(process.env.XDG_CONFIG_HOME, "nvim");
+        const nvim_data_dir = path.join(process.env.XDG_DATA_HOME, "nvim");
+
         try {
                 fs.mkdirSync(nvim_conf_dir, { recursive: true });
-                fs.mkdirSync(nvim_conf_dir, { recursive: true });
+                fs.mkdirSync(nvim_data_dir, { recursive: true });
         } catch (e) {
                 console.error("Failed to create config/data dirs");
         }
+
         init_vim = path.join(nvim_conf_dir, "init.vim");
+
         return resetVimrc();
-};
+}
 
 export function resetVimrc() {
         return writeVimrc(`set rtp+=${process.cwd()}\n`);
@@ -29,12 +39,11 @@ export function readVimrc() {
                 throw new Error("readVimrc called without setupVimrc!");
         }
         return fs.readFileSync(init_vim).toString();
-};
+}
 
 export function writeVimrc(content: string) {
         if (init_vim === undefined) {
                 throw new Error("writeVimrc called without setupVimrc!");
         }
         return fs.writeFileSync(init_vim, content);
-};
-
+}
